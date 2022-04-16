ab87090d81c1e0239b89dcc28ea6e51eed2aa80e
Dlgaston <dlgastonusmc@gmail.com>
2022-04-16 10:18:53 -0500

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,28 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div >
+      <header className="header-container">
+        <div>
+          Logo
+        </div>
+        <div className = "divbuttonborder">
+        Create Account
+        </div>
+        <div className="divbuttonborder">
+          Sign in
+        </div>
+      </header>
+
+      <body >
+      <div className ="body-container">
+    <h1>Hello</h1>
+      </div>
+      </body >
+    </div >
+
+  );
+}
+
+export default Home;
