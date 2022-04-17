2b87f49582f8ee5ef79005cf5527988e9fd4b438
Dlgaston <dlgastonusmc@gmail.com>
2022-04-17 17:33:59 -0500

    Update Layout.js

---
+++ src/components/Home/Layout.js
@@ -0,0 +1,30 @@
+import './Layout.css';
+
+function Layout() {
+  return (
+    <div >
+      <header>
+        <div className="header-container">
+        <div className="header-item header-item1">logo</div>
+        <div className="header-item header-item2">create-account</div>
+        <div className="header-item header-item3">sign-in</div>
+        </div>
+
+      </header>
+
+    <body>
+      <div className="flexbox-container">
+        <div className="flexbox-item flexbox-item1" ></div>
+        <div className="flexbox-item flexbox-item2" > Logo
+          <div className="logo">Logo</div>
+        </div>
+        <div className="flexbox-item flexbox-item3" ></div>
+      </div>
+      
+    </body>
+    </div >
+
+  );
+}
+
+export default Layout;
